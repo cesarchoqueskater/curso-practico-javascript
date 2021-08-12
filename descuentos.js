dd8c96ae264a8e647f215a225807a97923e463f0
Cesar Choquehuanca <cesarchoqueskater@gmail.com>
2021-08-12 00:50:14 -0500

    Logica de descuento en HTML

--- descuentos.js
+++ descuentos.js
@@ -17,7 +17,16 @@ console.log({
 
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
-    const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento) / 100;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
     return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice").value;
+    const inputDiscount = document.getElementById("InputDiscount").value;
+
+    const precioConDescuento = calcularPrecioConDescuento(inputPrice, inputDiscount);
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son " + precioConDescuento;
 }
