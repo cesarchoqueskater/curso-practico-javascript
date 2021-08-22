58e810e2bb261e0d346ca59fc347393d0bc95437
cchoqueh <cesar.choquehuanca@entel.pe>
2021-08-22 16:46:49 -0500

    Calculando el top 10% de salarios

--- analisis.js
+++ analisis.js
@@ -1,30 +1,10 @@
-const salariosCol = colombia.map(
-    function(personita) {
-        return personita.salary;
-    }
-);
-
-const salariosColSorted = salariosCol.sort(
-    function(salaryA, salaryB) {
-        return salaryA - salaryB;
-    }
-);
-
+// Funciones Helpers
 function esPar(numerito) {
     //Retornara true or false, sea el caso
     return (numerito % 2 === 0);
 }
 
-function calcularMediaAritmetica(lista) {
-    const sumaLista = lista.reduce(
-        function(valorAcumulado = 0, nuevoElemento) {
-            return valorAcumulado + nuevoElemento;
-        }
-    );
-    const promedioLista = sumaLista / lista.length;
-    return promedioLista;
-}
-
+// Calculadora de Mediana
 function medianaSalarios(lista) {
     const mitad = parseInt(lista.length / 2);
 
@@ -40,6 +20,41 @@ function medianaSalarios(lista) {
     }
 }
 
+// Calculando la Mediana General
+const salariosCol = colombia.map(
+    function(personita) {
+        return personita.salary;
+    }
+);
+
+const salariosColSorted = salariosCol.sort(
+    function(salaryA, salaryB) {
+        return salaryA - salaryB;
+    }
+);
+
+function calcularMediaAritmetica(lista) {
+    const sumaLista = lista.reduce(
+        function(valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+}
+
+const medianaGeneralCol = medianaSalarios(salariosColSorted);
+
+// Mediana del top 10%
+// splice -> Resive 2 argumentos, el primero la posicion donde empezara, y el otro argumento es la cantidad de elementos que va a tomar
+const spliceStart = (salariosColSorted.length * 90) / 100;
+const spliceCount = salariosColSorted.length - spliceStart;
+
+const salariosColTop10 = salariosColSorted.splice(spliceStart, spliceCount);
+
+const medianaTop10Col = medianaSalarios(salariosColTop10);
+
 console.log(
-    medianaSalarios(salariosColSorted)
+    medianaGeneralCol,
+    medianaTop10Col,
 );
