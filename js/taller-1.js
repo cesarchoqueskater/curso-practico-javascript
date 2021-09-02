6d34dacd9b74216ceb9da35d75505d27ea3022ad
cchoqueh <cesarchoqueskater@gmail.com>
2021-09-02 01:15:37 -0500

    js Figuras Geometricas

--- js/taller-1.js
+++ js/taller-1.js
@@ -1,9 +1,17 @@
-const $calculatePerimetro = document.getElementById("calculate-perimetro");
-const $calculateArea = document.getElementById("calculate-area");
+const $calculatePerimetroCuadrado = document.getElementById("calcPerimetroCuadrado");
+const $calculateAreaCuadrado = document.getElementById("calcAreaCuadrado");
+const $calculatePerimetroTriangulo = document.getElementById("calcPerimetroTriangulo");
+const $calculateAreaTriangulo = document.getElementById("calcAreaTriangulo");
+const $calculatePerimetroCircunferencia = document.getElementById("calcPerimetroCircunferencia");
+const $calculateAreaCircunferencia = document.getElementById("calcAreaCircunferencia");
+const $calculateDiametroCircunferencia = document.getElementById("calcDiametroCircunferencia");
 const $resultSquare = document.getElementById("resultsquare");
+const $resultTriangle = document.getElementById("resultTriangle");
+const $resultCircumference = document.getElementById("resultCircumference");
+const PI = Math.PI;
 
-
-$calculatePerimetro.addEventListener("click", function() {
+//Cuadrado
+$calculatePerimetroCuadrado.addEventListener("click", function() {
     const $sideSquare = document.getElementById("sideSquare").value;
 
     if ($sideSquare == 0) {
@@ -14,7 +22,7 @@ $calculatePerimetro.addEventListener("click", function() {
     }
 });
 
-$calculateArea.addEventListener("click", function() {
+$calculateAreaCuadrado.addEventListener("click", function() {
     const $sideSquare = document.getElementById("sideSquare").value;
 
     if ($sideSquare == 0) {
@@ -31,4 +39,61 @@ function calcularPerimetro(lado) {
 
 function areaCuadrado(lado) {
     return lado * lado;
+}
+
+//Triangulo
+$calculatePerimetroTriangulo.addEventListener("click", function() {
+    const $ladoATriangle = document.getElementById("sideATriangle").value;
+    const $ladoBTriangle = document.getElementById("sideBTriangle").value;
+    const $baseCTriangle = document.getElementById("baseTriangle").value;
+
+    if ($ladoATriangle == 0 || $ladoBTriangle == 0 || $baseCTriangle == 0) {
+        $resultTriangle.innerHTML = '<h3 class="result-error">Ningun lado del Triangulo puede quedar vacio o ser igual a 0</h3>';
+    } else {
+        const $resultado = perimetroTriangulo(parseInt($ladoATriangle), parseInt($ladoBTriangle), parseInt($baseCTriangle));
+        $resultTriangle.innerHTML = '<h3 class="result-ok">El perímetro es ' + $resultado + '</h3>';
+    }
+});
+
+$calculateAreaTriangulo.addEventListener("click", function() {
+    const $baseCTriangle = document.getElementById("baseTriangle").value;
+    const $heightTriangle = document.getElementById("heightTriangle").value;
+
+    if ($baseCTriangle == 0 || $heightTriangle == 0) {
+        $resultTriangle.innerHTML = '<h3 class="result-error">La base o altura no pueden estar vacios o ser igual a 0</h3>';
+    } else {
+        const $resultado = areaTriangulo(parseInt($baseCTriangle), parseInt($heightTriangle));
+        $resultTriangle.innerHTML = '<h3 class="result-ok">El área es ' + $resultado + '</h3>';
+    }
+});
+
+function perimetroTriangulo(ladoA, ladoB, base) {
+    return ladoA + ladoB + base;
+}
+
+function areaTriangulo(base, altura) {
+    return (base * altura) / 2;
+}
+
+//Circunferencia
+
+$calculatePerimetroCircunferencia.addEventListener("click", function() {
+    const $radio = document.getElementById("radio").value;
+
+    if ($radio == 0) {
+        $resultCircumference.innerHTML = '<h3 class="result-error">El radio no puede estar vacio o ser igual que 0</h3>';
+    } else {
+        const $resultado = perimetroCirculo(parseInt($radio));
+        $resultCircumference.innerHTML = '<h3 class="result-ok">El área es ' + $resultado + '</h3>';
+    }
+});
+
+//Diametro
+function diametroCirculo(radio) {
+    return radio * 2;
+}
+
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
 }
