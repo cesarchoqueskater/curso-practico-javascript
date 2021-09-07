33c4af7e827f78720b47f58f1ee7568af9ab13b3
cchoqueh <cesarchoqueskater@gmail.com>
2021-09-06 23:45:41 -0500

    Se agrega logica para taller2.js

---
+++ js/taller-2.js
@@ -0,0 +1,22 @@
+const getPriceDiscount = document.getElementById('getPriceDiscount');
+const resultPriceDiscount = document.getElementById('resultPriceDiscount');
+
+function calculatePriceDiscount(price, discount) {
+    const porcentPrecioConDescuento = 100 - discount;
+    const priceDiscount = (price * porcentPrecioConDescuento) / 100;
+    return priceDiscount;
+};
+
+
+getPriceDiscount.addEventListener("click", function() {
+    const priceData = document.getElementById('price').value;
+    const discountData = document.getElementById('discount').value;
+
+
+    if (priceData == 0 || discountData == 0) {
+        resultPriceDiscount.innerHTML = '<h3 class="result-error">El campo precio y descuento no pueden quedar vacios</h3>';
+    } else {
+        const getPriceDiscount = calculatePriceDiscount(priceData, discountData);
+        resultPriceDiscount.innerHTML = '<h3 class="result-ok">El descuento es ' + getPriceDiscount + '</h3>';
+    }
+});
